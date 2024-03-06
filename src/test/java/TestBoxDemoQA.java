d9417a4098e4407df27b5b5a18bcbf4a9f224844
Maxim Kazakov <info@kazakovms.ru>
2024-03-06 22:22:03 +0500

    add: TestBoxDemoQA

---
+++ src/test/java/TestBoxDemoQA.java
@@ -0,0 +1,39 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.conditions.clipboard.Content;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestBoxDemoQA {
+    //https://demoqa.com/text-box
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void fillForm() {
+        open("https://demoqa.com/text-box");
+//        Configuration.holdBrowserOpen = true;
+
+        $("#userName").setValue("Maxim Kazakov");
+        $("#userEmail").setValue("info@kazakovms.ru");
+        $("#currentAddress").setValue("First adress");
+        $("#permanentAddress").setValue("Second adress");
+        $("#submit").click();
+
+        $("#output").shouldHave(text("Maxim Kazakov"), text("info@kazakovms.ru"), text("First adress"),
+                text("Second adress"));
+
+
+    }
+
+    @AfterAll
+    static void settingProg() {
+        Configuration.holdBrowserOpen = true;
+    }
+}
