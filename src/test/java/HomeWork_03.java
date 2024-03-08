80e3f3396eb5a8a356639b3aa1b1d32a36d3ee4c
Maxim Kazakov <info@kazakovms.ru>
2024-03-08 21:09:09 +0500

    fix: HomeWork_03

--- src/test/java/HomeWork_03.java
+++ src/test/java/HomeWork_03.java
@@ -10,38 +10,51 @@ import java.io.File;
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class HomeWork_03 {
     //https://demoqa.com/automation-practice-form
     @BeforeAll
     static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.browser = "chrome";
         Configuration.browserPosition = "0x0";
         Configuration.browserSize = "1920x1080";
-        open("https://demoqa.com/automation-practice-form");
 
     }
 
     @Test
     void PracticeForm() {
 
+        open("/automation-practice-form");
+        executeJavaScript("$('#RightSide_Advertisement').remove()");
+
         $("#firstName").setValue("Maxim");
         $("#lastName").setValue("Kazakov");
         $("#userEmail").setValue("info@kazakovms.ru");
-        $("#gender-radio-2").parent().click();
+
+        $("#genterWrapper").$(byText("Female")).click(); //самый ТОП
+        //$("#gender-radio-2").parent().click(); //оптимальный
+        //$(byText("Male")).click(); //не очень хорошо, если на разных языках
+        //$("label[for=gender-radio-3]").click(); //оптимальный
+
         $("#userNumber").setValue("79000265003");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("May");
         $(".react-datepicker__year-select").selectOption("1980");
         $(".react-datepicker__day--010:not(.react-datepicker__day--outside-month)").click();
-        $("#subjectsInput").setValue("Intro");
+
+        $("#subjectsInput").setValue("Math").pressEnter();
+
         $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#hobbiesWrapper").$(byText("Reading")).click();
         $("#hobbiesWrapper").$(byText("Music")).click();
-        $("#uploadPicture").uploadFile(new File("src\\test\\resources\\1.jpg"));
+
+        $("#uploadPicture").uploadFromClasspath("src/test/resources/1.jpg");//Самый ТОП
+        //$("#uploadPicture").uploadFile(new File("src/test/resources/1.jpg"));
+
         $("#currentAddress").setValue("Kazancevo");
+
         $("#state").click();
         $("#stateCity-wrapper").$(byText("NCR")).click();
         $("#city").click();
